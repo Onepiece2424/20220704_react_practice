b36070be6b2e2c13a0e6ead6a13278c4ba3339ea
Onepiece2424 <79039664+Onepiece2424@users.noreply.github.com>
2022-07-05 05:24:18 +0900

    配列の値の追加と変更

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
-const obj1 = {
-  name: "主田",
-  age: 24
-};
+const arr1 = ["dog", "cat"];
+console.log(arr1);
 
-console.log(obj1);
+arr1[0] = "bird";
+console.log(arr1);
+
+arr1.push("monkey");
+console.log(arr1);
