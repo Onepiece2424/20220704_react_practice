026cc9e9b891fbbb65197b79f3ec1657d9ed62a9
Onepiece2424 <79039664+Onepiece2424@users.noreply.github.com>
2022-07-05 06:26:42 +0900

    アロー関数

--- src/index.js
+++ src/index.js
@@ -2,8 +2,12 @@
 //   return value;
 // }
 
-const func1 = function (value) {
+// const func1 = function (value) {
+//   return value;
+// };
+
+const func2 = (value) => {
   return value;
 };
 
-console.log(func1("func1です。"));
+console.log(func2("func2です。"));
