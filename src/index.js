f1819fd2e500bcbf153748c4d3a2f54713ec0dcd
Onepiece2424 <79039664+Onepiece2424@users.noreply.github.com>
2022-07-05 06:31:11 +0900

    引数の（）を省略したアロー関数

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
+//従来の関数（例1）
 // function func1(value) {
 //   return value;
 // }
 
+//従来の関数（例２）
 // const func1 = function (value) {
 //   return value;
 // };
 
+//通常のアロー関数
+// const func2 = (value) => {
+//   return value;
+// };
+
+//（）を省略したアロー関数
 const func2 = (value) => {
   return value;
 };
