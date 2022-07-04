adf03c80ae569b2e3f626856da913c516638e360
Onepiece2424 <79039664+Onepiece2424@users.noreply.github.com>
2022-07-05 06:34:47 +0900

    引数が2個以上の時のアロー関数の書き方

--- src/index.js
+++ src/index.js
@@ -14,8 +14,14 @@
 // };
 
 //（）を省略したアロー関数
-const func2 = (value) => {
-  return value;
+// const func2 = (value) => {
+//   return value;
+// };
+// console.log(func2("func2です。"));
+
+//引数が２つ以上の時のアロー関数
+const func3 = (value1, value2) => {
+  return value1 + value2;
 };
 
-console.log(func2("func2です。"));
+console.log(func3(1, 2));
