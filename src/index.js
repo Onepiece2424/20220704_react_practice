a38d0c77664f6320148d5a5822fadfed8122b066
Onepiece2424 <79039664+Onepiece2424@users.noreply.github.com>
2022-07-05 19:53:21 +0900

    スプレッド構文

--- src/index.js
+++ src/index.js
@@ -1,26 +1,4 @@
-//メッセージを出力する関数の書き方
-// const sayHello = (name) => console.log(`こんにちは！${name}さん。`);
-// sayHello("主田");
-
-//引数が渡されなかった時
-// const sayHello = (name = "ゲスト") => console.log(`こんにちは！${name}さん。`);
-// sayHello();
-// sayHello("田中");
-
-//存在しないプロパティの出力
-// const myProfile = {
-//   age: 24
-// };
-
-// const { name } = myProfile;
-
-// const message = `こんにちは！${name}さん。`;
-// console.log(message);
-
-//分割代入時にデフォルト値の設定
-const myProfile = {
-  age: 24
-};
-const { name = "田中" } = myProfile;
-const message = `こんにちは！${name}さん！`;
-console.log(message);
+//スプレッド構文
+const arr1 = [1, 2];
+console.log(arr1);
+console.log(...arr1);
