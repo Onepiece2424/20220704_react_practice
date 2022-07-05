8e5184d520a46cca7aa86a1f17aaee1000210ef8
Onepiece2424 <79039664+Onepiece2424@users.noreply.github.com>
2022-07-05 19:39:52 +0900

    デフォルト値の設定

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
 //メッセージを出力する関数の書き方
-const sayHello = (name) => console.log(`こんにちは！${name}さん。`);
-sayHello("主田");
+// const sayHello = (name) => console.log(`こんにちは！${name}さん。`);
+// sayHello("主田");
 
+//引数が渡されなかった時
+const sayHello = (name = "ゲスト") => console.log(`こんにちは！${name}さん。`);
+sayHello();
+sayHello("田中");
