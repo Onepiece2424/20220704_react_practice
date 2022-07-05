c55d5bde02384805f764f8ce0823113bd01ff155
Onepiece2424 <79039664+Onepiece2424@users.noreply.github.com>
2022-07-05 20:37:01 +0900

    分割代入時に要素をまとめる

--- src/index.js
+++ src/index.js
@@ -2,3 +2,12 @@
 const arr1 = [1, 2];
 console.log(arr1);
 console.log(...arr1);
+
+//分割代入時に要素をまとめる
+const arr2 = [1, 2, 3, 4, 5];
+
+const [num1, num2, ...arr3] = arr2;
+
+console.log(num1);
+console.log(num2);
+console.log(...arr3);
