d68fb43650c8e7c8fbfe529da72a88ace44c431d
Onepiece2424 <79039664+Onepiece2424@users.noreply.github.com>
2022-07-05 21:13:45 +0900

    スプレッド構文の要素のコピーと結合

--- src/index.js
+++ src/index.js
@@ -1,13 +1,24 @@
 //スプレッド構文
-const arr1 = [1, 2];
-console.log(arr1);
-console.log(...arr1);
+// const arr1 = [1, 2];
+// console.log(arr1);
+// console.log(...arr1);
 
-//分割代入時に要素をまとめる
-const arr2 = [1, 2, 3, 4, 5];
+// //分割代入時に要素をまとめる
+// const arr2 = [1, 2, 3, 4, 5];
 
-const [num1, num2, ...arr3] = arr2;
+// const [num1, num2, ...arr3] = arr2;
 
-console.log(num1);
-console.log(num2);
-console.log(...arr3);
+// console.log(num1);
+// console.log(num2);
+// console.log(...arr3);
+
+//スプレッド構文の要素のコピーと結合
+const arr4 = [10, 20];
+const arr5 = [30, 40];
+const arr6 = [...arr4];
+
+console.log(arr4);
+console.log(arr6);
+
+const arr7 = [...arr4, ...arr5];
+console.log(arr7);
